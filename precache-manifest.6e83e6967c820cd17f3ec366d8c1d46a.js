177b9a56be2c3e3cac044794e20e1d33aba1d048
Andrew Cruz <75159360+cruizeship@users.noreply.github.com>
2026-07-12 03:04:28 -0700

    Updates

---
+++ precache-manifest.6e83e6967c820cd17f3ec366d8c1d46a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc01dabac7f8133ee279d71a32e5e91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe4a646486491c1bd081",
+    "url": "/static/css/main.59411352.chunk.css"
+  },
+  {
+    "revision": "133c91331b3a68396947",
+    "url": "/static/js/2.1f3ce5e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1f3ce5e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4a646486491c1bd081",
+    "url": "/static/js/main.c6065a7f.chunk.js"
+  },
+  {
+    "revision": "3f54bbff68f722eeaa7d",
+    "url": "/static/js/runtime-main.19feda34.js"
+  },
+  {
+    "revision": "d672cb33118c242bbd4fa08ac830e093",
+    "url": "/static/media/Applelogo.d672cb33.png"
+  },
+  {
+    "revision": "077dc9fafc7aeea5c3fd64f8d647e10d",
+    "url": "/static/media/Carlmontlogo.077dc9fa.jpg"
+  },
+  {
+    "revision": "a3f8d05285b0d272a91d09c49f28c529",
+    "url": "/static/media/Chellelogo.a3f8d052.png"
+  },
+  {
+    "revision": "5d8b2c4dbe0cd6c5b66c2b8149a7c032",
+    "url": "/static/media/Improperlogo.5d8b2c4d.png"
+  },
+  {
+    "revision": "4e8a3f678a5dab327e61a26b25f96e1b",
+    "url": "/static/media/Kickbacklogo.4e8a3f67.png"
+  },
+  {
+    "revision": "0230c5970a0807e58cf3a3514577dd6c",
+    "url": "/static/media/Lockheedlogo.0230c597.png"
+  },
+  {
+    "revision": "ee960d9ba9a5bf3dfdccc1a7c34d1c1d",
+    "url": "/static/media/Retunelogo.ee960d9b.png"
+  },
+  {
+    "revision": "b20b56743bda40e36449e3396a6318bb",
+    "url": "/static/media/UCLAlogo.b20b5674.png"
+  },
+  {
+    "revision": "14c304826b1952bebb454c2dabf2900c",
+    "url": "/static/media/app.14c30482.png"
+  },
+  {
+    "revision": "a4cca655764b0837c919f5e6f281318f",
+    "url": "/static/media/app2.a4cca655.png"
+  },
+  {
+    "revision": "085524b95e8e41727cc1597426626295",
+    "url": "/static/media/app3.085524b9.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "30b2c5983f971a88e4457cc8dc994202",
+    "url": "/static/media/headshot.30b2c598.png"
+  },
+  {
+    "revision": "2e0ab5a545e1759972ffceb098a2e5a5",
+    "url": "/static/media/polaroid-10.2e0ab5a5.png"
+  },
+  {
+    "revision": "e1731ffe7ffb88a9d9c2caf3d95f6ed4",
+    "url": "/static/media/polaroid-11.e1731ffe.png"
+  },
+  {
+    "revision": "9c31b5b2338f66745792fa47c0b7d5fe",
+    "url": "/static/media/polaroid-12.9c31b5b2.png"
+  },
+  {
+    "revision": "bdf871eabd84e2fa709e1eb0c1382120",
+    "url": "/static/media/polaroid-13.bdf871ea.png"
+  },
+  {
+    "revision": "ed39083f922d5fc83b2cfff6312ab38d",
+    "url": "/static/media/polaroid-14.ed39083f.png"
+  },
+  {
+    "revision": "c9a911f59227fa5c092cee11d52dcd46",
+    "url": "/static/media/polaroid-15.c9a911f5.png"
+  },
+  {
+    "revision": "87d66e629de3d8066ab0b4d2ba7fb274",
+    "url": "/static/media/polaroid-4.87d66e62.png"
+  },
+  {
+    "revision": "0c4f6819ea20db49c079df5e4b5ee681",
+    "url": "/static/media/polaroid-5.0c4f6819.png"
+  },
+  {
+    "revision": "c40d2c03e8e72688362195e6d8c8befa",
+    "url": "/static/media/polaroid-6.c40d2c03.png"
+  },
+  {
+    "revision": "98080ce661ba2f1f70f570aecfb191f5",
+    "url": "/static/media/polaroid-7.98080ce6.png"
+  },
+  {
+    "revision": "15941640b376cbfe8b4a74fd4ad89e3a",
+    "url": "/static/media/polaroid-8.15941640.png"
+  },
+  {
+    "revision": "37afc2ac0553d73b3378b90b7fd065b9",
+    "url": "/static/media/polaroid-9.37afc2ac.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdcc21b9e23721b12a8b9aa85d99fe60.js"
+  "/precache-manifest.6e83e6967c820cd17f3ec366d8c1d46a.js"
 );
 
 self.addEventListener('message', (event) => {
